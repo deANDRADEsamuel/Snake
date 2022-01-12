230b5f52466708638257e6213b9ff4fb1f6e874e
deANDRADEsamuel <90719747+deANDRADEsamuel@users.noreply.github.com>
2022-01-12 09:46:20 +0100

    Update Snake.py

--- Snake.py
+++ Snake.py
@@ -33,11 +33,11 @@ snake_surface.fill((255, 255, 255))
 snake_direction = K_LEFT
 
   #Pomme
-pomme_surface = pygame.Surface((TAILLE_PIXEL, TAILLE_PIXEL)) #Taille
-pomme_surface.fill((255, 0, 0))  #Couleur rouge
+pomme_surface = pygame.Surface((TAILLE_PIXEL, TAILLE_PIXEL))
+pomme_surface.fill((255, 0, 0))
 pomme_pos = random_on_grid()
 
-  #Quand on meurt, on revient à la case départ
+  #En mourant, le jeu recommence
 def restart_game():
     global snake_pos
     global pomme_pos
@@ -48,26 +48,26 @@ def restart_game():
     
   #Pour que le programme ne se ferme pas
 while True:
-    pygame.time.Clock().tick(15)  #Déplacement
-    screen.fill((0, 0, 0))  #background noir
-    for event in pygame.event.get():  #Lister tous les événements dans le jeu
-        if event.type == QUIT:  #quit = evento saida do jogo, também pode ser escrito pygame.locals.QUIT
+    pygame.time.Clock().tick(15)
+    screen.fill((0, 0, 0))
+    for event in pygame.event.get():
+        if event.type == QUIT:
             pygame.quit()
             quit()
         elif event.type == KEYDOWN:
-            if event. key in [K_UP, K_DOWN, K_LEFT, K_RIGHT]:  #Pour que seul ces touches là fonctionnent, ex: l'espace ne marcherai pas
+            if event. key in [K_UP, K_DOWN, K_LEFT, K_RIGHT]:
                 snake_direction = event.key
     
-    screen.blit(pomme_surface, pomme_pos)  #Para aparecer na tela, esta antes da cobra pra a cobra passar por cima da maça e nao o contrario
+    screen.blit(pomme_surface, pomme_pos)  #La pomme apparaît sur l'écran
 
     if collision(pomme_pos, snake_pos[0]):  #en touchant la pomme, le serpent grandit
         snake_pos.append((-10, -10))
-        pomme_pos = random_on_grid()  #la pote, touchée, réapparait aléatoirement
+        pomme_pos = random_on_grid()  #la pomme, touchée, réapparait aléatoirement
             
     for pos in snake_pos:
-        screen.blit(snake_surface, pos)  #Blit é para desenhar na tela
+        screen.blit(snake_surface, pos)
         
-    for i in range(len(snake_pos)-1, 0, -1): #Posiçao (como tamanho) começando da cauda ate a cabeça, pegar na nova peça e igualar la na posiçao da peça interior. 1er numero no py é 0, quer dizer que ultimo elemento de 10 seria 9; o -1 està aqui para nao ter esse problema. Ajuda na fluides quando muda de direçao.
+    for i in range(len(snake_pos)-1, 0, -1):
         if collision(snake_pos[0], snake_pos[i]):
             restart_game()
         snake_pos[i] = snake_pos[i-1]
